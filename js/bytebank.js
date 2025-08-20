1fa2e73a0585c60bed22115fe2bf4ecf9e69f8ea
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-08-20 17:47:08 -0300

    feat: solução com javascript

---
+++ js/bytebank.js
@@ -0,0 +1,13 @@
+let saldo = 3000
+
+const elementoSaldo = document.querySelector('.saldo-valor .valor')
+elementoSaldo.textContent = saldo
+
+const elementoFormulario = document.querySelector('.block-nova-transacao form')
+elementoFormulario.addEventListener('submit', (event) => {
+  event.preventDefault()
+  if (!elementoFormulario.checkValidity()) {
+    alert('Por favor, preencha todos os campos da transação!')
+    return
+  }
+})
