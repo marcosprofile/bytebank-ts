474a6574e8004f344b070d57d8905630a3330752
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-08-21 01:10:46 -0300

    refactor: modularizando o typescript

---
+++ src/components/nova-transacao-component.ts
@@ -0,0 +1,31 @@
+import { Transacao } from "../types/Transacao.js"
+import { TipoTransacao } from "../types/TipoTransacao.js"
+import Conta from "../types/Conta.js"
+import SaldoComponent from "./saldo-component.js"
+
+const elementoFormulario = document.querySelector<HTMLFormElement>('.block-nova-transacao form')
+elementoFormulario.addEventListener('submit', (event) => {
+  event.preventDefault()
+  if (!elementoFormulario.checkValidity()) {
+    alert('Por favor, preencha todos os campos da transação!')
+    return
+  }
+
+  const inputTipoTransacao = elementoFormulario.querySelector<HTMLSelectElement>('#tipoTransacao')
+  const inputValor = elementoFormulario.querySelector<HTMLInputElement>('#valor')
+  const inputData = elementoFormulario.querySelector<HTMLInputElement>('#data')
+
+  let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao
+  let valor: number = inputValor.valueAsNumber
+  let data: Date = new Date(inputData.value)
+
+  const novaTransacao: Transacao = {
+    tipoTransacao: tipoTransacao,
+    valor: valor,
+    data: data
+  }
+
+  Conta.registrarTransacao(novaTransacao)
+  SaldoComponent.atualizar()
+  elementoFormulario.reset()
+})
---
+++ src/components/saldo-component.ts
@@ -0,0 +1,25 @@
+import { formatarData, formatarMoeda } from "../utils/formatters.js"
+import { FormatoData } from "../types/FormatoData.js"
+import Conta from "../types/Conta.js"
+
+const elementoSaldo = document.querySelector<HTMLElement>('.saldo-valor .valor')
+const elementoDataAcesso = document.querySelector<HTMLElement>('.block-saldo time')
+
+if (elementoDataAcesso !== null) {
+  elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO)
+}
+
+renderizarSaldo()
+function renderizarSaldo(): void {
+  if (elementoSaldo !== null) {
+    elementoSaldo.textContent = formatarMoeda(Conta.getSaldo())
+  }
+}
+
+const SaldoComponent = {
+  atualizar() {
+    renderizarSaldo()
+  }
+}
+
+export default SaldoComponent
