3b2546a36885c260005ee6738702a4613933c77c
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-08-22 00:30:46 -0300

    refactor: aplicando orientação a objetos

--- src/types/Conta-antiga.ts
+++ src/types/Conta-antiga.ts
@@ -2,23 +2,46 @@ import { GrupoTransacao } from "./GrupoTransacao.js"
 import { TipoTransacao } from "./TipoTransacao.js"
 import { Transacao } from "./Transacao.js"
 
-export class Conta {
-  nome: string
-  saldo: number = JSON.parse(localStorage.getItem('saldo')) || 0
-  transacoes: Transacao[] = JSON.parse(localStorage.getItem('transacoes'), (key: string, value: any) => {
-    if (key === 'data') {
-      return new Date(value)
-    }
-    return value
-  }) || []
-
-  constructor(nome: string) {
-    this.nome = nome
+let saldo: number = JSON.parse(localStorage.getItem('saldo')) || 0
+const transacoes: Transacao[] = JSON.parse(localStorage.getItem('transacoes'), (key: string, value: string) => {
+  if (key === 'data') {
+    return new Date(value)
   }
+
+  return value
+}) || []
+
+function debitar(valor: number): void {
+  if (valor <= 0) {
+    throw new Error('O valor a ser debitado deve ser maior que zero!')
+  }
+  if (valor > saldo) {
+    throw new Error('Saldo insulficiente!')
+  }
+  saldo -= valor
+  localStorage.setItem('saldo', saldo.toString())
+}
+
+function depositar(valor: number): void {
+  if (valor <= 0) {
+    throw new Error('O valor a ser depositado deve ser maior que zero!')
+  }
+  saldo += valor
+  localStorage.setItem('saldo', saldo.toString())
+}
+
+const Conta = {
+  getSaldo() {
+    return saldo
+  },
+
+  getDataAcesso() {
+    return new Date()
+  },
 
   getGruposTransacoes(): GrupoTransacao[] {
     const gruposTransacoes: GrupoTransacao[] = []
-    const listaTransacoes: Transacao[] = structuredClone(this.transacoes)
+    const listaTransacoes: Transacao[] = structuredClone(transacoes)
     const transacoesOrdenadas: Transacao[] = [...listaTransacoes].sort((t1, t2) => t2.data.getTime() - t1.data.getTime())
     let labelAtualGrupoTransacao: string = ''
 
@@ -35,51 +58,22 @@ export class Conta {
     }
 
     return gruposTransacoes
-  }
-
-  getSaldo() {
-    return this.saldo
-  }
-
-  getDataAcesso(): Date {
-    return new Date()
-  }
+  },
 
   registrarTransacao(novaTransacao: Transacao): void {
     if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-      this.depositar(novaTransacao.valor)
+      depositar(novaTransacao.valor)
     } else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
-      this.debitar(novaTransacao.valor)
+      debitar(novaTransacao.valor)
       novaTransacao.valor *= -1
     } else {
       throw new Error('Tipo de Transação inválido!')
     }
 
-    this.transacoes.push(novaTransacao)
+    transacoes.push(novaTransacao)
     console.log(this.getGruposTransacoes())
-    localStorage.setItem('transacoes', JSON.stringify(this.transacoes))
-  }
-
-  debitar(valor: number): void {
-    if (valor <= 0) {
-      throw new Error('O valor a ser debitado deve ser maior que zero!')
-    }
-    if (valor > this.saldo) {
-      throw new Error('Saldo insulficiente!')
-    }
-    this.saldo -= valor
-    localStorage.setItem('saldo', this.saldo.toString())
-  }
-
-  depositar(valor: number): void {
-    if (valor <= 0) {
-      throw new Error('O valor a ser depositado deve ser maior que zero!')
-    }
-    this.saldo += valor
-    localStorage.setItem('saldo', this.saldo.toString())
+    localStorage.setItem('transacoes', JSON.stringify(transacoes))
   }
 }
 
-const conta = new Conta('Marcos L. Ferreira')
-
-export default conta
+export default Conta
